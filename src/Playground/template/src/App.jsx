ca92b70055ec077691853537e3f6fb2544b8f2fd
fewismuch <779205344@qq.com>
2023-10-24 13:52:37 +0000

    Update 3 files

--- src/Playground/template/src/App.jsx
+++ src/Playground/template/src/App.jsx
@@ -6,14 +6,10 @@ function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>Hello React Playground</h1>
       <div className='card'>
         <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
     </>
   )
 }
