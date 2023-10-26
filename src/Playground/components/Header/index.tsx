bd828b2c8e21f533305c04d8e286758868157198
fewismuch <779205344@qq.com>
2023-10-26 13:26:04 +0000

    fix: 更新下载和复制图标

--- src/Playground/components/Header/index.tsx
+++ src/Playground/components/Header/index.tsx
@@ -1,10 +1,11 @@
-import React, { useContext, useMemo } from 'react'
+import React, { useContext, useState } from 'react'
 
 import DownloadSvg from './icons/download.svg?raw'
 import GithubSvg from './icons/github.svg?raw'
 import MoonSvg from './icons/moon.svg?raw'
 import ReactSvg from './icons/react.svg'
 import ShareSvg from './icons/share.svg?raw'
+import SuccessSvg from './icons/success.svg?raw'
 import SunSvg from './icons/sun.svg?raw'
 import { PlaygroundContext } from '../../PlaygroundContext'
 import { downloadFiles } from '../../utils'
@@ -13,66 +14,70 @@ import styles from './index.module.less'
 
 export const Header: React.FC = () => {
   const { files, theme, changeTheme } = useContext(PlaygroundContext)
+  const [copyed, setCopyed] = useState(false)
+  const [downloaded, setDownloaded] = useState(false)
 
   const copyLink = async () => {
     await navigator.clipboard.writeText(location.href)
-    alert('Sharable URL has been copied to clipboard.')
+    setCopyed(true)
+    setTimeout(() => {
+      setCopyed(false)
+    }, 3000)
   }
 
   const downloadProject = () => {
-    if (!confirm('Download project files?')) {
-      return
-    }
-    downloadFiles(files).then(() => console.log('download success'))
+    downloadFiles(files).then(() => {
+      setDownloaded(true)
+      setTimeout(() => {
+        setDownloaded(false)
+      }, 3000)
+    })
   }
 
-  return useMemo(
-    () => (
-      <nav className={styles.header}>
-        <div className={styles.logo}>
-          <img alt='logo' src={ReactSvg} />
-          <span>React Playground</span>
-        </div>
-        <div className={styles.links}>
-          {theme === 'light' && (
-            <button
-              title='Toggle dark mode'
-              className={styles.theme}
-              dangerouslySetInnerHTML={{ __html: SunSvg }}
-              onClick={() => changeTheme('dark')}
-            />
-          )}
-          {theme === 'dark' && (
-            <button
-              title='Toggle light mode'
-              className={styles.theme}
-              dangerouslySetInnerHTML={{ __html: MoonSvg }}
-              onClick={() => changeTheme('light')}
-            />
-          )}
-
+  return (
+    <nav className={styles.header}>
+      <div className={styles.logo}>
+        <img alt='logo' src={ReactSvg} />
+        <span>React Playground</span>
+      </div>
+      <div className={styles.links}>
+        {theme === 'light' && (
           <button
-            title='Copy sharable URL'
-            dangerouslySetInnerHTML={{ __html: ShareSvg }}
-            onClick={copyLink}
+            title='Toggle dark mode'
+            className={styles.theme}
+            dangerouslySetInnerHTML={{ __html: SunSvg }}
+            onClick={() => changeTheme('dark')}
           />
-
+        )}
+        {theme === 'dark' && (
           <button
-            title='Download project files'
-            dangerouslySetInnerHTML={{ __html: DownloadSvg }}
-            onClick={downloadProject}
+            title='Toggle light mode'
+            className={styles.theme}
+            dangerouslySetInnerHTML={{ __html: MoonSvg }}
+            onClick={() => changeTheme('light')}
           />
+        )}
 
-          <a
-            href='https://github.com/fewismuch/react-playground'
-            target='_blank'
-            title='View on GitHub'
-          >
-            <button dangerouslySetInnerHTML={{ __html: GithubSvg }} />
-          </a>
-        </div>
-      </nav>
-    ),
-    [theme]
+        <button
+          title='Copy sharable URL'
+          dangerouslySetInnerHTML={{ __html: copyed ? SuccessSvg : ShareSvg }}
+          onClick={copyLink}
+        />
+
+        <button
+          title='Download project files'
+          dangerouslySetInnerHTML={{ __html: downloaded ? SuccessSvg : DownloadSvg }}
+          onClick={downloadProject}
+        />
+
+        <a
+          href='https://github.com/fewismuch/react-playground'
+          target='_blank'
+          title='View on GitHub'
+        >
+          <button dangerouslySetInnerHTML={{ __html: GithubSvg }} />
+        </a>
+      </div>
+    </nav>
   )
 }
