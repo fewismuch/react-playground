b0f0561fc908d62952d33734eaafd220c61c0b3b
fewismuch <779205344@qq.com>
2023-11-04 05:08:42 +0000

    feat: 新增支持ts

--- src/Playground/files.ts
+++ src/Playground/files.ts
@@ -1,27 +1,27 @@
 import importMap from './template/import-map.json?raw'
 import AppCss from './template/src/App.css?raw'
-import App from './template/src/App.jsx?raw'
-import main from './template/src/main.jsx?raw'
+import App from './template/src/App.tsx?raw'
+import main from './template/src/main.tsx?raw'
 import { getFilesFromUrl } from './utils'
 
 import type { IFiles } from './types'
 
 // app文件名
-export const MAIN_FILE_NAME = 'App.jsx'
+export const MAIN_FILE_NAME = 'App.tsx'
 // esm模块映射文件名
 export const IMPORT_MAP_FILE_NAME = 'import-map.json'
 // app入口文件名
-export const ENTRY_FILE_NAME = 'main.jsx'
+export const ENTRY_FILE_NAME = 'main.tsx'
 
 export const initFiles: IFiles = getFilesFromUrl() || {
   [ENTRY_FILE_NAME]: {
     name: ENTRY_FILE_NAME,
-    language: 'javascript',
+    language: 'typescript',
     value: main,
   },
   [MAIN_FILE_NAME]: {
     name: MAIN_FILE_NAME,
-    language: 'javascript',
+    language: 'typescript',
     value: App,
   },
   'App.css': {
@@ -39,7 +39,7 @@ export const initFiles: IFiles = getFilesFromUrl() || {
 export const reactTemplateFiles = {
   [ENTRY_FILE_NAME]: {
     name: ENTRY_FILE_NAME,
-    language: 'javascript',
+    language: 'typescript',
     value: main,
   },
   [IMPORT_MAP_FILE_NAME]: {
