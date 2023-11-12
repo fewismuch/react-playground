a67ae53b0fa1abf73d194fd62c98bf8740a411c6
tianyu <779205344@qq.com>
2023-11-12 17:46:43 +0800

    fix: 更新示例

--- src/example/Demo1.tsx
+++ src/example/Demo1.tsx
@@ -1,30 +1,9 @@
 import { Playground } from '../Playground'
 
 export const Demo1 = () => {
-  const files = {
-    'App.tsx': `import {title} from './const'
-function App() {
-  return <h1>this is {title}</h1>
-}
-export default App
-`,
-    'const.ts': {
-      code: 'export const title = "demo2"',
-    },
-  }
-
   return (
     <>
-      <h1>作为组件使用：</h1>
-      <Playground
-        showHeader={false}
-        showCompileOutput={false}
-        fileSelectorReadOnly
-        width={700}
-        height={400}
-        files={files}
-        border
-      />
+      <Playground />
     </>
   )
 }
---
+++ src/example/Demo2.tsx
@@ -0,0 +1,48 @@
+import { PlaygroundSandbox } from '../Playground'
+
+export const Demo2 = () => {
+  const files = {
+    'App.tsx': `import {title} from './const'
+function App() {
+  return <h1>this is {title}</h1>
+}
+export default App
+`,
+    'const.ts': {
+      code: 'export const title = "demo2"',
+    },
+  }
+
+  return (
+    <>
+      <h1>作为组件使用：</h1>
+      <PlaygroundSandbox
+        showHeader={false}
+        showCompileOutput={false}
+        fileSelectorReadOnly
+        width={700}
+        height={400}
+        border
+        theme='dark'
+        options={{
+          lineNumbers: false,
+        }}
+      />
+      <div style={{ height: '60vh' }}></div>
+      <div>滚动到可视范围内才会加载</div>
+      <PlaygroundSandbox
+        showHeader={false}
+        showCompileOutput={false}
+        fileSelectorReadOnly
+        width={700}
+        height={400}
+        files={files}
+        border
+        theme='dark'
+        options={{
+          lineNumbers: false,
+        }}
+      />
+    </>
+  )
+}
