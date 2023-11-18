51eecbc597985bb712d1da003c706a4fdf9da5b3
tianyu <779205344@qq.com>
2023-11-18 14:46:39 +0800

    fix: 版本更新至0.1.9

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { PlaygroundSandbox } from './Playground/PlaygroundSandbox'
+// import { Playground as PlaygroundSandbox } from '@/Playground'
 
 function App() {
   const handleFilesHash = (hash: string) => {
